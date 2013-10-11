0ea477c88fe255d6f146cae2f02fc623e6f553ec
Rec9 <recneuf@gmail.com>
2013-10-11 14:21:11 +0200

    .

--- bruit.ml
+++ bruit.ml
@@ -8,3 +8,14 @@ let to_grey img =
 					Sdlvideo.put_pixel img i j c
 			done
 		done
+
+(* 
+Gris: quand r=g=b // gris "foncé" quand inf à 180.
+On va donc virer ceux étant sup à 180 en les passant en blanc.
+ *)
+
+let whiterthanwhite img =
+	let (w, h) = Utile.get_dim img in
+		for i = 0 to w-1 do
+			for j = 0 to h-1 do
+				let 
