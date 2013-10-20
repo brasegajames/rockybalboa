a79ed3dfd9649fa254d1e92b2e75d221d8d62907
brasegajames <brasegajames@hotmail.fr>
2013-10-20 19:00:28 +0200

    rotation hough 2 différentes manières non fonctionelles

--- rotation.ml
+++ rotation.ml
@@ -1,3 +1,4 @@
+
 (* moyenne *)
 
 (* Somme de chaque composante *)
@@ -33,8 +34,116 @@ let slope l =
 
 
 
+let rec listClean l (min,max) = match l with
+  |[] -> []
+  |(x,_)::t when (x == min) -> listClean t (min,max)
+  |(x,_)::t when (x == max) -> listClean t (min,max)
+  |h::t -> h :: listClean t (min,max)
 
-(* Renvoie les coordonnées d'un point après rotation d'angle a autour de x0, y0 *)
+
+let giveMinMax = function
+| [] -> failwith "Liste vide"
+| (x, _)::t -> 
+  let rec getmM min max = function
+    |[] -> (min,max)
+    |(x,_)::t when x < min -> getmM x max t
+    |(x,_)::t when x > max -> getmM min x t
+    |h::t -> getmM min max t
+    in
+      getmM x x t
+
+let nbPoints = 200
+
+let add_min a l = 
+  let (x1, y1) = a.(0) in
+  let min = ref x1 and y = ref y1 in
+    for i = 1 to 4 do
+      let (x2, y2) = a.(i) in
+        if x2 != 0 && x2 < !min then
+        (
+          min := x2;
+          y := y2;
+        )
+      done;
+      if !min = 0 then l else (!min, !y) :: l
+    
+
+let get_points m = 
+let l = ref [] and x = ref 0 and  y = ref 0
+  and w = Array.length m and h = Array.length m.(0) in
+  let dy = max (h / nbPoints) 1 and arr = Array.make 5 (0, 0) in
+    while !y < h - 5 * dy do
+      for i = 0 to 4 do
+        x := 0;
+        arr.(i) <- (0, 0);
+        while !x < w/4 do
+          if (m.(!x).(!y)) then
+          begin
+            arr.(i) <- (!x, !y);
+            x := w;
+          end;
+          x := !x + 1;
+        done;
+      y := !y + dy;
+      done;
+      l := add_min arr !l;
+    done;
+    l
+
+
+(* detecte l'angle de rotation *)
+let angleDetection m =
+  let l = get_points m in
+  let a = ref (slope !l) in
+  let a1 = !a in
+    let l2 = ref (listClean !l (giveMinMax !l)) in
+      let a2 = ref (slope !l2) in
+        while !a /. !a2 > 1.1 || !a /. !a2 < 0.9 do
+          l := !l2;
+          a := !a2;
+          a2 := slope (listClean !l (giveMinMax !l))
+        done;
+  if atan a1 < 0.2 & atan a1 > -0.2 then
+    atan a1 (* angle initial trouve quasi nul *)
+  else
+          (-. atan (1. /. !a))
+        
+
+
+let hough mat = 
+        let pi = acos(-1.) and pi02 = asin(1.) in
+        let (w,h) = (Array.length mat,Array.length mat.(0)) in
+        let diagonal = int_of_float(sqrt(float_of_int(w*w+ h*h))) in
+        let matrice_de_vote = Array.make_matrix diagonal ((int_of_float(pi*.100.))+1) 0 in
+        let teta_max = ref 0. in
+        let vote_max = ref 0  in
+        for y=0 to h-1 do
+           for x=0 to w-1 do
+              if (mat.(x).(y) == false) then
+                begin
+                let t = ref(-.pi02) in           
+                 while ( !t <= pi02 ) do
+                  let droite =int_of_float((float x *. (cos !t))+.(float
+                  y *. (sin !t))) in
+                       if droite>=0 then 
+                         begin
+                          let teta_i = int_of_float(!t*.100. +. pi02*.100.)in
+                           matrice_de_vote.(droite).(teta_i) <-
+                                  matrice_de_vote.(droite).(teta_i)+1;
+                           if !vote_max < matrice_de_vote.(droite).(teta_i) then
+                                  begin
+                                  vote_max := matrice_de_vote.(droite).(teta_i);
+                                  teta_max := !t;
+                                  end;
+                         end;
+                         t := !t +. 0.01;
+                 done;
+                end;
+           done;
+        done;
+        (!teta_max +. (pi02 /. 2.))
+
+
 let get_rotated_coord x y x0 y0 cosa sina = 
   let a = ((float x) -. x0) *. cosa -. ((float y) -. y0) *. sina +. x0
   and b = ((float x) -. x0) *. sina +. ((float y) -. y0) *. cosa +. y0 in
@@ -69,18 +178,6 @@ let floatMat t =
       m
 
 
-(* transforme une matrice d'entier en une matrice de booléens (seuil : 1) *)
-let boolMat t =
-  let w = Array.length t and h = Array.length t.(0) in
-    let m = Array.make_matrix w h false in
-      for i = 0 to w-1 do
-        for j = 0 to h-1 do
-          if t.(i).(j) > 0 then
-            m.(i).(j) <- true;
-        done;
-      done;
-      m
-
 
 (* Fais tourner une image de l'angle a *)
 let rotation img a =
@@ -98,3 +195,10 @@ let rotation img a =
 
 
 
+
+
+
+(* Détecte et fais tourner l'image *)
+let rotate m =
+  let a = (hough m) in
+    rotation m a
--- rotation.mli
+++ rotation.mli
@@ -3,6 +3,13 @@ val average : (int * int) list -> float * float
 val numOfSlope : float -> float -> (int * int) list -> float
 val denumOfSlope : float -> (int * 'a) list -> float
 val slope : (int * int) list -> float
+val listClean : ('a * 'b) list -> 'a * 'a -> ('a * 'b) list
+val giveMinMax : ('a * 'b) list -> 'a * 'a
+val nbPoints : int
+val add_min : (int * 'a) array -> (int * 'a) list -> (int * 'a) list
+val get_points : bool array array -> (int * int) list ref
+val angleDetection : bool array array -> float
+val hough : bool array array -> float
 val get_rotated_coord :
   int -> int -> float -> float -> float -> float -> float * float
 val get_linear_interpolation :
@@ -10,5 +17,5 @@ val get_linear_interpolation :
   bool array array ->
   int -> int -> int -> int -> float -> float -> float -> float -> unit
 val floatMat : bool array array -> float array array
-val boolMat : int array array -> bool array array
 val rotation : bool array array -> float -> bool array array
+val rotate : bool array array -> bool array array
